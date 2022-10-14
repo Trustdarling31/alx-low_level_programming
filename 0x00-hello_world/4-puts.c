3d86ab2a90fa6704ffe1a1dc3c8b27d36ec9ca3a
Trustdarling31 <darltrust3132@gmail.com>
2022-10-13 17:18:17 -0700

    creates 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
   * main - Printing
   * Return: Always 0 (Successful)
   */
- int main(void)
- {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
- }
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
