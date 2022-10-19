93ef4420d622425def4a00fcbfbd70e7ff70308c
Trustdarling31 <darltrust3132@gmail.com>
2022-10-19 07:38:33 -0700

    3-islower.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _islower - Entry point
+  * Description: checks is a character is lowercase
+  * @c: the integer value it receives
+  * Return: 1 if true.0 if false
+  */
+int _islower(int c)
+{
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
