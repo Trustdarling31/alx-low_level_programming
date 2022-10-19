13cd58f536a6360a6bd9ccc88d0c32fe4d640643
Trustdarling31 <darltrust3132@gmail.com>
2022-10-19 13:34:45 -0700

    6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _abs - Entry point
+  * @t: is an integer
+  * Description: prints the absolute value of an integer
+  * Return: the absolute value
+  */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
