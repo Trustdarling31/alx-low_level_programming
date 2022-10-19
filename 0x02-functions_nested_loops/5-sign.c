13bc60cc9807e329438dcfbe8213a4a93eb050c5
Trustdarling31 <darltrust3132@gmail.com>
2022-10-19 13:49:40 -0700

    5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
   * print_sign - Entry point
   * @n: carrier variable
   * Description: prints the sign of a number
-  * Return: 1 if n>0, 0 if n == 0,-1 if n<0
+  * Return: 1 if n>0, 0 if n == 0, -1 if n<0
   */
 int print_sign(int n)
 {
@@ -13,14 +13,13 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
+		_putchar('-');
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return ('0');
 }
-
