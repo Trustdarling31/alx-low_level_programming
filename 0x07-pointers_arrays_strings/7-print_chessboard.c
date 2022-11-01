637d0061611ae62c4dc54a787ab80178987fb195
Trustdarling31 <darltrust3132@gmail.com>
2022-10-31 18:56:15 -0700

    7-print_chessboard.c file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,15 +3,17 @@
 /**
   * print_chessboard - prints a chessboard
   * @a: the chessboard to be printed
+  * Return: void
   */
 void print_chessboard(char (*a)[8])
 {
-	int indx1, indx2;
+	int x;
+	int y;
 
-	for (indx1 = 0; a[indx1][7]; indx1++)
+	for (x = 0; x < 8; x++)
 	{
-		for (indx2 = 0; indx2 < 8; indx2++)
-			_putchar(a[indx1][indx2]);
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
 
 		_putchar('\n');
 	}
