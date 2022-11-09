b4354b7243ef51e88014ae1781e9c74ed86d8517
Trustdarling31 <darltrust3132@gmail.com>
2022-11-09 04:50:39 -0800

    100-change.c file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,4 +26,17 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
+
+	while (coins[position] != '\0')
+	{
+		if (total >= coins[position])
+		{
+			aux = (total / coins[position]);
+			change += aux;
+			total -= coins[position] * aux;
+		}
+		position++;
+	}
+	printf("%d\n", change);
+	return (0);
 }
